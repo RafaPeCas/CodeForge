5bc83db2efe7595e0a23672fd8a11485a065a0f2
Rafa <blas_tois@alumnos.ilernasevilla.com>
2025-02-25 13:10:38 +0100

    .

--- CodeForge/resources/js/Components/ui/tooltip.tsx
+++ CodeForge/resources/js/Components/ui/tooltip.tsx
@@ -1,6 +1,4 @@
-"use client"
-
-import { ComponentProps } from "react"
+import * as React from "react"
 import * as TooltipPrimitive from "@radix-ui/react-tooltip"
 
 import { cn } from "@/lib/utils"
@@ -8,7 +6,7 @@ import { cn } from "@/lib/utils"
 function TooltipProvider({
   delayDuration = 0,
   ...props
-}: ComponentProps<typeof TooltipPrimitive.Provider>) {
+}: React.ComponentProps<typeof TooltipPrimitive.Provider>) {
   return (
     <TooltipPrimitive.Provider
       data-slot="tooltip-provider"
@@ -20,7 +18,7 @@ function TooltipProvider({
 
 function Tooltip({
   ...props
-}: ComponentProps<typeof TooltipPrimitive.Root>) {
+}: React.ComponentProps<typeof TooltipPrimitive.Root>) {
   return (
     <TooltipProvider>
       <TooltipPrimitive.Root data-slot="tooltip" {...props} />
@@ -30,7 +28,7 @@ function Tooltip({
 
 function TooltipTrigger({
   ...props
-}: ComponentProps<typeof TooltipPrimitive.Trigger>) {
+}: React.ComponentProps<typeof TooltipPrimitive.Trigger>) {
   return <TooltipPrimitive.Trigger data-slot="tooltip-trigger" {...props} />
 }
 
@@ -39,7 +37,7 @@ function TooltipContent({
   sideOffset = 4,
   children,
   ...props
-}: ComponentProps<typeof TooltipPrimitive.Content>) {
+}: React.ComponentProps<typeof TooltipPrimitive.Content>) {
   return (
     <TooltipPrimitive.Portal>
       <TooltipPrimitive.Content

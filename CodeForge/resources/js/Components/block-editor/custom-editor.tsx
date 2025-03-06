f1fe67b67bf89f43b705bc3397d1eaae19100a10
RafaPeCas <rpedcast@gmail.com>
2025-03-06 21:07:35 +0100

    Lowlight

--- CodeForge/resources/js/Components/block-editor/custom-editor.tsx
+++ CodeForge/resources/js/Components/block-editor/custom-editor.tsx
@@ -1,8 +1,14 @@
 import { useState, ChangeEvent } from "react";
 import Markdown from "marked-react";
+import Lowlight from "react-lowlight";
+import javascript from 'highlight.js/lib/languages/javascript';
+import 'highlight.js/styles/default.css'
 
 export const MarkdownEditor = () => {
+    Lowlight.registerLanguage("js", javascript);
     const [markdownText, setMarkdownText] = useState("# HOLAA");
+    const code = `const hello = "Hola Mundo"; console.log(hello);`;
+    const value = `<textarea id="MarkdownArea" value={markdownText} onChange={update} className="border p-2 w-full h-32"/>`;
 
     function update(event: ChangeEvent<HTMLTextAreaElement>) {
         setMarkdownText(event.target.value);
@@ -19,6 +25,8 @@ export const MarkdownEditor = () => {
             <div className="prose dark:prose-invert">
                 <Markdown>{markdownText}</Markdown>
             </div>
+            <Lowlight language="js" value={code} markers={[]} />
+            <Lowlight language="js" value={value} markers={[]} />
         </>
     );
 };

09e241aa115042f15dced8f5f39030d3cd7728fe
RafaPeCas <rpedcast@gmail.com>
2025-03-10 11:00:05 +0100

    highlight.js and markdown text

--- CodeForge/resources/js/Components/block-editor/custom-editor.tsx
+++ CodeForge/resources/js/Components/block-editor/custom-editor.tsx
@@ -2,12 +2,14 @@ import { useState, ChangeEvent } from "react";
 import Markdown from "marked-react";
 import Lowlight from "react-lowlight";
 import javascript from 'highlight.js/lib/languages/javascript';
+import java from 'highlight.js/lib/languages/vbscript-html';
 import 'highlight.js/styles/default.css'
 
 export const MarkdownEditor = () => {
     Lowlight.registerLanguage("js", javascript);
+    Lowlight.registerLanguage("java", java);
     const [markdownText, setMarkdownText] = useState("# HOLAA");
-    const code = `const hello = "Hola Mundo"; console.log(hello);`;
+    const code = `const code = const hello = "Hola Mundo"; console.log(hello);`;
     const value = `<textarea id="MarkdownArea" value={markdownText} onChange={update} className="border p-2 w-full h-32"/>`;
 
     function update(event: ChangeEvent<HTMLTextAreaElement>) {
@@ -26,7 +28,7 @@ export const MarkdownEditor = () => {
                 <Markdown>{markdownText}</Markdown>
             </div>
             <Lowlight language="js" value={code} markers={[]} />
-            <Lowlight language="js" value={value} markers={[]} />
+            <Lowlight language="java" value={value} markers={[]} />
         </>
     );
 };

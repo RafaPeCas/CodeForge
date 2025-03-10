b3662cf039ccba28406620986ed17e829d296727
RafaPeCas <rpedcast@gmail.com>
2025-03-10 11:33:47 +0100

    Ajustes en el merge

--- CodeForge/resources/js/Pages/Welcome.tsx
+++ CodeForge/resources/js/Pages/Welcome.tsx
@@ -1,6 +1,4 @@
 import { Button } from '@/Components';
-import NotebooksComponent from '@/Components/NotebooksComponent';
-import PageComponent from '@/Components/PageComponent';
 import { PageProps } from '@/types';
 import { Head, Link } from '@inertiajs/react';
 
@@ -365,8 +363,6 @@ export default function Welcome({
                 </div>
             </div>
             <Button>shadcn alive?</Button>
-            <NotebooksComponent />
-            <PageComponent />
         </>
     );
 }
